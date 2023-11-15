d846bdf0a9ee325e0127037eb922258081f3e8fa
graverealm <109730545+muzscripter@users.noreply.github.com>
2023-11-15 07:13:32 -0500

    Create Katana.lua

---
+++ Katana.lua
@@ -0,0 +1,86 @@
+local Module = {}
+
+local function Weld(A: BasePart, B: BasePart, Offset: CFrame, Name: string?)
+	A.CFrame = B.CFrame * Offset
+	
+	local ExistingWeld = A:FindFirstChild("WeldConstraint")
+	if ExistingWeld then
+		if ExistingWeld.Name == 'KatanaWeld' then
+			print('Found katana weld')
+			
+			ExistingWeld.Part0 = A
+			ExistingWeld.Part1 = B
+		end
+	else
+		local WeldConstraint = Instance.new('WeldConstraint')
+		WeldConstraint.Part0 = A
+		WeldConstraint.Part1 = B
+		WeldConstraint.Parent = A
+		WeldConstraint.Name = Name or string.format('Weld-%s', A.Name)
+	end
+end
+
+function Module.Setup(Player)
+	print('[SERVER] Attempting to setup katana')
+	
+	local Humanoid = Player.Character.Humanoid
+	local Character = Player.Character
+	local HumanoidRootPart = Character.HumanoidRootPart
+	
+	
+	if not Character:FindFirstChild('Katana') then
+		local Katana = (game:GetService('ReplicatedStorage'):WaitForChild('ref-graverealm')).Katana:Clone()
+		local Sheathe = (game:GetService('ReplicatedStorage'):WaitForChild('ref-graverealm3')).Sheathe:Clone()
+		local KatanaPart = Character:WaitForChild(Katana.Handle.ObjectValue.Value.Name)
+		local SheathePart = Character:WaitForChild(Sheathe.Handle.ObjectValue.Value.Name)
+		
+		Weld(Katana.Handle, KatanaPart, Katana.Handle.ObjectValue.Value.CFrame:Inverse() * Katana.Handle.CFrame, 'KatanaWeld')
+		Weld(Sheathe.Handle, SheathePart, Sheathe.Handle.ObjectValue.Value.CFrame:Inverse() * Sheathe.Handle.CFrame)
+
+		Katana.Parent = Character
+		Sheathe.Parent = Character
+	else
+		print('[SERVER] Player already has a katana')
+	end
+end
+
+function Module.Equip(Player)
+	local Humanoid = Player.Character.Humanoid
+	local Character = Player.Character
+	local HumanoidRootPart = Character.HumanoidRootPart
+	
+	if Character:FindFirstChild('Katana') then
+		local EquipTrack = Humanoid:LoadAnimation(game:GetService('ReplicatedStorage'):WaitForChild('EquipKatana'))
+		EquipTrack.Priority = Enum.AnimationPriority.Action4
+		
+		EquipTrack:Play()
+		
+		local Sound = game:GetService('ReplicatedStorage')["Sheathe / Unseathe"]:Clone()
+		Sound.PlayOnRemove = true
+		Sound.Volume = 1
+		Sound.Parent = Character
+		Sound:Destroy()
+		Sound = nil
+		
+		local Katana = Character:FindFirstChild('Katana')
+		local Katana2 = game:GetService('ReplicatedStorage'):WaitForChild('ref-graverealm2').Katana
+		local KatanaPart = Katana2:WaitForChild('Handle').ObjectValue.Value.Name
+		
+		local ExistingWeld = Katana.Handle:FindFirstChild("KatanaWeld")
+		if ExistingWeld then
+			
+			Katana.Handle.CFrame = KatanaPart * Katana.Handle.ObjectValue.Value.CFrame:Inverse() * Katana.Handle.CFrame
+			
+			ExistingWeld.Part0 = Katana.Handle
+			ExistingWeld.Part1 = KatanaPart
+		end
+		
+		Katana.Parent = Character
+	end
+	
+end
+
+function Module:Unequip()
+	
+end
+return Module
